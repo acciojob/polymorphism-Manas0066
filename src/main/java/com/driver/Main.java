ecb082f01cbaf36fff821d997f70b42ceea8aa7f
Manas0066 <manaschaudhary1602@gmail.com>
2025-05-14 13:19:59 +0530

    create project and improved some parts

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -16,8 +16,8 @@ public class Main {
            return x*y;
        }
    }
-    //dsa
-    public static void main(String[] args) {
+
+   public static void main(String[] args) {
         Product p = new Product();
         int ans1 = p.product(2,3);
 
